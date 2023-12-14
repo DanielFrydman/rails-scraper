4a7f45bb68d99362b207c9ba6681ce281f1b3773
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-14 15:42:40 -0300

    ✨ Creating a base page to start working on the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
+import { useState } from "react";
+import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputs, setInputs] = useState([""]);
+
+  const handleInputChange = (index, event) => {
+    const newInputs = [...inputs];
+    newInputs[index] = event.target.value;
+    setInputs(newInputs);
+  };
+
+  const handleAddInput = () => {
+    if (areInputsFilled) {
+      setInputs([...inputs, ""]);
+    }
+  };
+
+  const handleRemoveInput = (index) => {
+    const newInputs = [...inputs];
+    newInputs.splice(index, 1);
+    setInputs(newInputs);
+  };
+
+  const areInputsFilled = inputs.every((input) => input.trim() !== "");
+
+  const handleScrapeData = () => {
+    // Lógica para web scraping
+    // Exemplo: axios.post('/api/scrape', { inputs }).then(...);
+    if (areInputsFilled) {
+      console.log(inputs);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className="grid justify-items-center mt-10">
+        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-200 dark:border-gray-200 dark:hover:bg-gray-200">
+          <div className="group mt-3">
+            <input id="url_address" name="url_address" type="text" required />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>URL address</label>
+          </div>
+
+          {inputs.map((input, index) => (
+            <div key={index} className="group" id="fields">
+              <input
+                id="field"
+                name="field"
+                type="text"
+                value={input}
+                onChange={(event) => handleInputChange(index, event)}
+                required
+              />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+              <label>Field to extract</label>
+              <button
+                className="delete-icon"
+                onClick={() => handleRemoveInput(index)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+
+          <div className="flex justify-between button-div">
+            <button
+              className="bg-blue-500 w-32 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleAddInput}
+            >
+              Add Field
+            </button>
+            <button
+              className="bg-blue-500 w-32 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleScrapeData}
+            >
+              Start Scrap
+            </button>
+          </div>
+
+          {/* Renderize os resultados do scraping aqui */}
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+// componentes com pastas por componentes
+// pasta de api
+// criar index de componentes pra exportar tudo junto
