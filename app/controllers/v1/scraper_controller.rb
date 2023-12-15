046ccd956da2eca519a491b61e7f7bd354c44619
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-15 09:11:05 -0300

    ✨ Implementing the controller to use the web page scraper service

--- app/controllers/v1/scraper_controller.rb
+++ app/controllers/v1/scraper_controller.rb
@@ -3,7 +3,36 @@
 module V1
   class ScraperController < ApplicationController
     def index
-      byebug
+      service = WebPageScraperService.new(
+        url: url,
+        meta_tags: meta_tags,
+        css_selector_fields: css_selector_fields
+      )
+
+      render json: { result: service.scrape }, status: :ok
+    rescue WebPageScraperException,
+           HtmlFetcherException => e
+      render json: { error: e }, status: :bad_request
+    rescue StandardError => e
+      render json: { error: e }, status: :internal_server_error
+    end
+
+    private
+
+    def url
+      params.require(:scraper).require(:url)
+    end
+
+    def meta_tags
+      fields[:meta].to_a
+    end
+
+    def css_selector_fields
+      fields.except(:meta).to_h
+    end
+
+    def fields
+      params.require(:scraper).permit(fields: {})[:fields]
     end
   end
 end
