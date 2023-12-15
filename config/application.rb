81c4133ca12bd4345965d31f2df7b89c3a80fbf8
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-15 15:05:45 -0300

    ✨ Adding redis into application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module RailsScraper
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.cache_store = :redis_cache_store, { url: "redis://localhost:6379/0" }
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
