83320f9318ae5ecce1f8018f733552116dd98990
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-16 10:50:09 -0300

    📝 Configuring REDIS for test environment

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module RailsScraper
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    config.cache_store = :redis_cache_store, { url: "redis://redis:6379" }
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
