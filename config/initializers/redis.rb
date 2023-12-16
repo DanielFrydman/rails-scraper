0c9ac54d52ae89f20bea242683926d2f9e16c2a8
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-16 14:49:08 -0300

    🎨 Improving redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,20 +1,8 @@
 # frozen_string_literal: true
 
-Rails.application.config.redis_create = lambda do
-  if Rails.env.test?
-    db = ((ENV['TEST_ENV_NUMBER'].presence || '1').to_i - 1)
-    url = ENV.fetch('REDIS_TEST_URL', 'redis://localhost:6380')
-  else
-    db = 0
-    url = ENV.fetch('REDIS_URL', 'redis://localhost:6379')
-  end
-
-  Redis.new(
-    url: url,
-    db: db,
-    timeout: nil,
-    reconnect_attempts: nil
-  )
-end
-
-Rails.application.config.redis = Rails.application.config.redis_create.call
+Rails.application.config.redis = Redis.new(
+  url: APP_CONFIG::REDIS_URL,
+  db: 0,
+  timeout: nil,
+  reconnect_attempts: nil
+)
