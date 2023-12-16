f92a9b8debdb013e8e963bc95525fc838e64a4c9
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-16 14:48:26 -0300

    🎨 Removing usage of Dotenv and use AppConfig

---
+++ config/initializers/app_config.rb
@@ -0,0 +1,10 @@
+CONFIG = if Rails.env.production?
+           YAML.load_file(Rails.root.join('app.yml'))[Rails.env]
+         else
+           YAML.load_file(Rails.root.join('config/app.yml'))[Rails.env]
+         end
+
+module APP_CONFIG
+  PROXY_KEY = CONFIG['proxy_key']
+  REDIS_URL = CONFIG['redis_url']
+end
