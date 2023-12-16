cae138dbc6171b2d5815c20f5f76aaa03e006c59
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-16 09:32:16 -0300

    ✨ Adding Redis in the HtmlFetcherService to retrieve HTML faster

--- app/services/html_fetcher_service.rb
+++ app/services/html_fetcher_service.rb
@@ -4,24 +4,36 @@ require 'faraday'
 
 class HtmlFetcherService
   NOT_AUTHORIZED_TOKEN = 'AUTH'.freeze
+  CACHE_EXPIRATION_TIME = 30.minutes
+  TIME_NEEDED_TO_THE_PROXY_WORK_CORRECTLY = 180.freeze
 
   def initialize
-    @proxy = "http://#{Rails.application.config.proxy_key}:js_render=true&antibot=true@proxy.zenrows.com:8001"
+    @cache = Rails.application.config.redis
+    initialize_connection
   end
 
   def fetch(url:)
-    connection = Faraday.new(proxy: @proxy, ssl: { verify: false })
-    connection.options.timeout = 180
-    response = connection.get(url, nil, nil)
-    @response_body = response.body
-    raise_not_authorized_error if connection_not_authorized?
+    cached_html = @cache.get(url)
+    return cached_html if cached_html.present?
 
+    @response_body = @connection.get(url, nil, nil).body
+    raise_not_authorized_error if connection_not_authorized?
+  
+    @cache.setex(url, CACHE_EXPIRATION_TIME, @response_body)
     @response_body
   rescue StandardError => e
     raise_html_fetcher_exception(e)
   end
 
   private
+
+  def initialize_connection
+    @connection = Faraday.new(
+      proxy: "http://#{Rails.application.config.proxy_key}:js_render=true&antibot=true@proxy.zenrows.com:8001",
+      ssl: { verify: false }
+    )
+    @connection.options.timeout = TIME_NEEDED_TO_THE_PROXY_WORK_CORRECTLY
+  end
 
   def raise_not_authorized_error
     parsed_body = JSON.parse(@response_body)
