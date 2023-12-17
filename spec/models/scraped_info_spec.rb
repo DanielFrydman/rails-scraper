6ac849aabf44e2f9d2201362092ed495e2f7c751
Daniel Frydman <danielfrydman@id.uff.br>
2023-12-16 23:31:18 -0300

    ✅ Creating new spec for ScrapedInfo model

---
+++ spec/models/scraped_info_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ScrapedInfo, type: :model do
+ subject { FactoryBot.create(:scraped_info, url: Faker::Internet.url, data: { 'price' => '18290,-' }) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to(be_valid)
+  end
+
+  it "is not valid without a URL" do
+    subject.url = nil
+    expect(subject).to_not(be_valid)
+  end
+
+  it "is not valid without a DATA" do
+    subject.data = nil
+    expect(subject).to_not(be_valid)
+  end
+end
